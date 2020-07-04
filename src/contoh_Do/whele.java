263c0400fcba944697a4eeced77a7bf6d6697161
HP <HP@HP-PC>
2020-07-04 22:50:18 +0700

    ini projek pertama saya tolong bantuannya kalau masih salah terima kasih

---
+++ src/contoh_Do/whele.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contoh_Do;
+
+/**
+ *
+ * @author HP
+ */
+public class whele {
+      public static void main(String[] args) {
+         int n = 5;
+        String  mobil  = "";
+        System.out.println(" isi bensin ");
+        System.out.println(" nyalakan mesin ");
+        System.out.println(" oper gigi pasneleng ");
+        System.out.println(" jalan ");
+        
+       
+        mobil = "jalan";
+        System.out.println("apakah mobilmu sudah bisa ? ");
+        
+        if("jalan".equals(mobil)){
+             
+        }
+        else{   
+        
+    }
+        int i = 1;
+        do{
+            System.out.println("nyalakan mesin lagi " + 1);
+            i++;;
+        }
+            
+        while(i<=5);{
+            
+        }
+        System.out.println("selesai");
+                  
+        
+    }
+    
+}
---
+++ src/kenalan1/Kenalan.java
@@ -0,0 +1,48 @@
+
+ 
+package kenalan1;
+
+/**
+ *
+ * @author HP
+ */
+
+// HALLO NETIJEN YG BERIMAN
+// HALLO SEMUA
+
+/*
+BEBAS BERKOMENTAR WAHAI ORANG BERIMAN
+*/
+
+
+public class Kenalan {
+    public static void main(String[] args) {
+        
+
+        //BILANGAN BULAT    = INTEGER
+        //BILANGAN PECAHAN  = DOUBLE
+        //KARAKTER          = CHARAKTER
+        //TEKS              = STRING
+        
+        //TIPE DATA
+        
+        //VARIABEL
+        //KONSTANTA
+        String namanyaApa = "akuuuu" ;
+        int bilanganbulat = 5 ;
+        int x =50;
+        
+        
+         String nama = "DIMAS ISBANDI" ;
+         String ttl = "LAMONGAN, 14 JUNI 1997" ;
+         String status = "jomlo hahah" ;
+       
+        
+        System.out.println(nama) ;
+        System.out.println(ttl);
+        System.out.println(status);
+    }
+
+  
+            
+}
